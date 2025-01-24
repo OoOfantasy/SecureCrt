3033487577af92de8666034f9a037b96fe21554d
HeBao <2781523140@qq.com>
2025-01-24 15:18:15 +0800

    modified:   SecureCrt/Window.py

--- SecureCrt/Window.py
+++ SecureCrt/Window.py
@@ -3,11 +3,13 @@ class Window:
         self.crt = crt
 
     @property
-    def Active(self):
+    def Active(self) -> int:
+        """ 是否活动窗口, 1是, 0否 """
         return self.crt.Window.Active
 
     @property
-    def Caption(self):
+    def Caption(self) -> str:
+        """ 返回或设置窗口标题 """
         return self.crt.Window.Caption
 
     @Caption.setter
@@ -16,10 +18,13 @@ class Window:
 
     @property
     def State(self):
+        """ 窗口状态, 0隐藏, 1正常, 2最小化, 3最大化 """
         return self.crt.Window.State
 
     def Activate(self):
+        """ 置顶窗口 """
         self.crt.Window.Activate()
 
     def Show(self, state):
+        """ 显示窗口, state[0隐藏, 1正常, 2最小化, 3最大化] """
         self.crt.Window.Show(state)
